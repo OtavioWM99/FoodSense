827db5a8d105e0ad7b41b0b91940d1bcd5d69363
OtavioWM99 <otaviowerlang99@gmail.com>
2025-07-21 02:26:43 -0300

    feat: tela de receitas
    -Utilização de imagens para ícones nos botões
    -Adequacação de tamanho e margem para cada imagem e texto de cada botão

---
+++ app-foodsense/app/(tabs)/receitas.js
@@ -0,0 +1,120 @@
+import { View, Text, TouchableOpacity, ScrollView, Image } from 'react-native';
+import { Ionicons } from '@expo/vector-icons';
+import { LinearGradient } from 'expo-linear-gradient';
+import { useFonts } from 'expo-font';
+import { useEffect } from 'react';
+import { SafeAreaView } from 'react-native-safe-area-context';
+import { scale, verticalScale, moderateScale } from 'react-native-size-matters'; //biblioteca size-matters para responsividade
+
+
+export default function CardapioScreen() {
+
+  const [fontsLoaded] = useFonts({
+    'Poppins-Thin': require('../../assets/fonts/Poppins-Thin.ttf'),
+    'Poppins-ExtraLight': require('../../assets/fonts/Poppins-ExtraLight.ttf'),
+    'Poppins-Light': require('../../assets/fonts/Poppins-Light.ttf'),
+    'Poppins-Regular': require('../../assets/fonts/Poppins-Regular.ttf'),
+    'Poppins-Medium': require('../../assets/fonts/Poppins-Medium.ttf'),
+    'Poppins-SemiBold': require('../../assets/fonts/Poppins-SemiBold.ttf'),
+    'Poppins-Bold': require('../../assets/fonts/Poppins-Bold.ttf'),
+    'Poppins-ExtraBold': require('../../assets/fonts/Poppins-ExtraBold.ttf'),
+    'Poppins-Black': require('../../assets/fonts/Poppins-Black.ttf'),
+    'Poppins-BlackItalic': require('../../assets/fonts/Poppins-BlackItalic.ttf'),
+  });
+
+
+  return (
+
+    <SafeAreaView style={{ flex: 1 }}>
+      <LinearGradient 
+          colors={['#4ade80', '#14b8a6']}
+          className="flex-1"
+      >
+        <ScrollView showsVerticalScrollIndicator={false}>
+          {/* Cabeçalho */}
+          <View className=" bg-white flex-row justify-end pr-6">
+            <TouchableOpacity className="items-center">
+              <Ionicons name="person-sharp" color="black" style={{ fontSize: moderateScale(30) }} />
+              <Text style={{ fontSize: moderateScale(10.5) }} className="font-poppinsMedium"> Meu perfil</Text>
+            </TouchableOpacity>
+          </View>
+
+          {/* Título */}
+          <Text style={{ 
+              fontSize: moderateScale(20),
+              marginTop: verticalScale(40),
+            }} className="font-poppinsBold text-center text-white">
+            Encontre receitas com alimentos alternativos
+          </Text>
+
+          {/* Grade de botões */}
+          <View className="flex-row flex-wrap justify-evenly">
+            {[
+              { label: 'Sem glúten', image: require('../../assets/icons/gluten-free.png'), width: scale(75), height: scale(75), labelMarginBottom: moderateScale(1), labelMarginTop: moderateScale(1) },
+              { label: 'Sem lactose', image: require('../../assets/icons/lactose-free.png'), width: scale(90), height: scale(90), labelMarginBottom: moderateScale(10), labelMarginTop: moderateScale(-6) },
+              { label: 'Veganas', image: require('../../assets/icons/vegan.png' ), width: scale(65), height: scale(65), labelMarginBottom: moderateScale(-4), labelMarginTop: moderateScale(6) },
+              { label: 'Baixo teor de frutose', image: require('../../assets/icons/fructose-low.png'), width: scale(75), height: scale(75), labelMarginBottom: moderateScale(2), labelMarginTop: moderateScale(2) },
+            ].map((item, index) => (
+              <TouchableOpacity style={{
+                  backgroundColor: '#D9D9D9',
+                  width: scale(140),
+                  height: verticalScale(120),
+                  marginTop: moderateScale(20)
+                }}
+                key={index}
+                className="rounded-3xl justify-center items-center shadow"
+                activeOpacity={0.7}
+              >
+                <Image
+                    source={item.image}
+                    style={{
+                      width: item.width,
+                      height: item.height,
+                      resizeMode: 'contain',
+                    }}
+                />
+                <Text className="font-poppinsMedium text-center" style={{ 
+                    fontSize: moderateScale(12.5),
+                    marginBottom: item.labelMarginBottom,
+                    marginTop: item.labelMarginTop
+                  }}>
+                  {item.label}
+                </Text>
+              </TouchableOpacity>
+            ))}
+          </View>
+
+          {/* Botão Mudar preferências */}
+          <TouchableOpacity 
+            activeOpacity={0.7}
+            className="rounded-full flex-row items-center justify-center shadow" 
+            style={{
+              backgroundColor:'#D9D9D9',
+              padding: moderateScale(10),
+              marginTop: verticalScale(25),
+              marginLeft: scale (25),
+              width: scale(300)
+            }}> 
+            <Ionicons name="person-outline" style={{ fontSize:  moderateScale(23) }} color="black" />
+            <Text className="ml-2 font-poppinsMedium" style={{ fontSize:  moderateScale(12) }}>Mudar intolerâncias ou preferências</Text>
+          </TouchableOpacity>
+        </ScrollView>
+
+        {/* Navegação inferior */}
+        <View className="flex-row justify-around items-center bg-white py-3 border-t border-gray-200">
+          {[
+            { icon: 'home-outline', label: 'Home' },
+            { icon: 'restaurant-outline', label: 'Cardápio' },
+            { icon: 'book-outline', label: 'Receitas' },
+            { icon: 'newspaper-outline', label: 'Notícias' },
+          ].map((item, index) => (
+            <TouchableOpacity key={index} className="items-center">
+              <Ionicons name={item.icon} size={24} color="black" />
+              <Text className="text-xs">{item.label}</Text>
+            </TouchableOpacity>
+          ))}
+        </View>
+      </LinearGradient>
+    </SafeAreaView>
+  );
+}
