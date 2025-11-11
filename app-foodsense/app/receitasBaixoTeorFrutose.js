2204a6656d4e247748adad1aaad7ad42c2cd86dc
OtavioWM99 <otaviowerlang99@gmail.com>
2025-11-11 03:57:39 -0300

    feature: exclusão de itens e tela de carregamento
    -Agora é possível excluir cardapios e receitas clicando na lixeira
    
    -Adição de icone de carregamento enquanto cardapios e receitas ainda nao aparecem
    
    -Melhoria de feeback visual

--- app-foodsense/app/receitasBaixoTeorFrutose.js
+++ app-foodsense/app/receitasBaixoTeorFrutose.js
@@ -1,5 +1,5 @@
 import { useRouter } from "expo-router";
-import { Image, ScrollView, Text, TouchableOpacity, View } from "react-native";
+import { Image, ScrollView, Text, TouchableOpacity, View, Alert, ActivityIndicator } from "react-native";
 import { scale, verticalScale, moderateScale } from 'react-native-size-matters';
 import { shadowStyle } from "../src/components/Shadow";
 import { LinearGradient } from 'expo-linear-gradient';
@@ -13,6 +13,7 @@ import { useEffect, useState } from "react";
 export default function ReceitasBaixoTeorFrutose() {
     const router = useRouter();
     const [receitas, setReceitas] = useState([]);
+    const [loading, setLoading] = useState(true);
 
     useEffect(() => {
         const fetchReceitas = async () => {
@@ -26,10 +27,41 @@ export default function ReceitasBaixoTeorFrutose() {
             } else {
                 setReceitas(data);
             }
+            setLoading(false);
         };
 
         fetchReceitas();
     }, []);
+
+    const handleDelete = (itemId) => {
+        Alert.alert(
+          "Confirmar Exclusão",
+          "Tem certeza que deseja excluir este item? Esta ação é irreversível.",
+          [
+            {
+              text: "Voltar",
+              style: "cancel"
+            },
+            {
+              text: "Confirmar",
+              onPress: async () => {
+                const { error } = await supabase
+                  .from('receitas_salvas')
+                  .delete()
+                  .eq('id', itemId);
+  
+                if (error) {
+                  console.error('Erro ao excluir:', error);
+                  Alert.alert("Erro", "Não foi possível excluir o item.");
+                } else {
+                  setReceitas(currentReceitas => currentReceitas.filter(item => item.id !== itemId));
+                }
+              },
+              style: "destructive"
+            }
+          ]
+        );
+    };
 
     return (
         <LinearGradient
@@ -60,46 +92,50 @@ export default function ReceitasBaixoTeorFrutose() {
                         </Text>
 
 
-                        <View style={{ marginTop: verticalScale(20), width: '100%' }}>
-                            {receitas.map(refeicao => (
-                                <TouchableOpacity 
-                                    key={refeicao.id} 
-                                    style={[
-                                        {
-                                            flexDirection: 'row',
-                                            alignItems: 'center',
-                                            backgroundColor: '#F5F5F5',
-                                            borderRadius: moderateScale(10),
-                                            padding: moderateScale(10),
-                                            marginBottom: verticalScale(15),
-                                        }
-                                    ]}
-                                    activeOpacity={0.7}
-                                    onPress={() => router.push(`/detalhe-item/${refeicao.id}?table=receitas_salvas`)}
-                                >
-                                    {/* <Image source={refeicao.imagem} style={{ width: scale(80), height: verticalScale(80), borderRadius: moderateScale(10) }} /> */}
-                                    <View style={{ marginLeft: moderateScale(15), flex: 1 }}>
-                                        <Text style={{
-                                            fontSize: moderateScale(16),
-                                            fontFamily: 'Poppins-SemiBold',
-                                        }}>{refeicao.nome}</Text>
-                                        <View style={{ flexDirection: 'row', marginTop: verticalScale(10) }}>
-                                            {/* {refeicao.icones.map((icone, index) => (
-                                                <Image key={index} source={icone} style={{ width: scale(24), height: verticalScale(24), marginRight: moderateScale(10) }} />
-                                            ))} */}
+                        {loading ? (
+                            <ActivityIndicator size="large" color="white" style={{ marginVertical: verticalScale(50) }} />
+                        ) : (
+                            <View style={{ marginTop: verticalScale(20), width: '100%' }}>
+                                {receitas.map(refeicao => (
+                                    <TouchableOpacity 
+                                        key={refeicao.id} 
+                                        style={[
+                                            {
+                                                flexDirection: 'row',
+                                                alignItems: 'center',
+                                                backgroundColor: '#F5F5F5',
+                                                borderRadius: moderateScale(10),
+                                                padding: moderateScale(10),
+                                                marginBottom: verticalScale(15),
+                                            }
+                                        ]}
+                                        activeOpacity={0.7}
+                                        onPress={() => router.push(`/detalhe-item/${refeicao.id}?table=receitas_salvas`)}
+                                    >
+                                        {/* <Image source={refeicao.imagem} style={{ width: scale(80), height: verticalScale(80), borderRadius: moderateScale(10) }} /> */}
+                                        <View style={{ marginLeft: moderateScale(15), flex: 1 }}>
+                                            <Text style={{
+                                                fontSize: moderateScale(16),
+                                                fontFamily: 'Poppins-SemiBold',
+                                            }}>{refeicao.nome}</Text>
+                                            <View style={{ flexDirection: 'row', marginTop: verticalScale(10) }}>
+                                                {/* {refeicao.icones.map((icone, index) => (
+                                                    <Image key={index} source={icone} style={{ width: scale(24), height: verticalScale(24), marginRight: moderateScale(10) }} />
+                                                ))} */}
+                                            </View>
                                         </View>
-                                    </View>
-                                    <View style={{ flexDirection: 'row' }}>
-                                        <TouchableOpacity style={{ padding: moderateScale(5) }} activeOpacity={0.7}>
-                                            <Ionicons name="create-outline" size={moderateScale(24)} color="black" />
-                                        </TouchableOpacity>
-                                        <TouchableOpacity style={{ padding: moderateScale(5) }} activeOpacity={0.7}>
-                                            <Ionicons name="trash-outline" size={moderateScale(24)} color="black" />
-                                        </TouchableOpacity>
-                                    </View>
-                                </TouchableOpacity>
-                            ))}
-                        </View>
+                                        <View style={{ flexDirection: 'row' }}>
+                                            <TouchableOpacity style={{ padding: moderateScale(5) }} activeOpacity={0.7} onPress={(e) => {
+                                                e.stopPropagation();
+                                                handleDelete(refeicao.id);
+                                            }}>
+                                                <Ionicons name="trash-outline" size={moderateScale(24)} color="black" />
+                                            </TouchableOpacity>
+                                        </View>
+                                    </TouchableOpacity>
+                                ))}
+                            </View>
+                        )}
 
                         <NovoCardapioButton onPress={() => router.push('/assistente')} />
 
